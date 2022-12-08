3e0a980c67ea5f683739b7bc2c14c09618b6452e
Rachel Helmich <101941048+midwestbotanist@users.noreply.github.com>
2022-12-07 22:04:01 -0600

    Adding more styling

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -40,7 +40,12 @@ L.geoJSON(data, {
                 return L.circleMarker(latlng);
             },
           // We set the style for each circleMarker using our styleInfo function.
-        style: styleInfo
+        style: styleInfo,
+        // We create a popup for each circleMarker to display the magnitude and
+        //  location of the earthquake after the marker has been created and styled.
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+            }
         }).addTo(map);
     });
     
