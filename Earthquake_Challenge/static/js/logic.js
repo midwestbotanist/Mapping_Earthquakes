b6d1d1e94a6e4fc5f6b1ca1ad33ca2e48a6b50c5
Rachel Helmich <101941048+midwestbotanist@users.noreply.github.com>
2022-12-10 20:11:46 -0600

    Adding challenge to repo, deliverable 1 complete

---
+++ Earthquake_Challenge/static/js/logic.js
@@ -0,0 +1,48 @@
+// We create the tile layer that will be the background of our map.
+let day = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-guidance-day-v4/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let night = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Day: day,
+  Night: night
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [44.0, -80.0],
+  zoom: 2,
+  layers: [day]
+})
+
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the airport GeoJSON URL
+let torontoData = "https://raw.githubusercontent.com/midwestbotanist/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+    color: "#ffffa1",
+    weight: 2,
+    onEachFeature: function(feature, layer) {
+          console.log(layer);
+          layer.bindPopup("<h2>" + "Airport code: " + feature.properties.airline + "</h2> <hr> <h2>" + "Airport name: " + feature.properties.dst + "</h2>" );
+        }
+  }
+    ).addTo(map);
+  });
